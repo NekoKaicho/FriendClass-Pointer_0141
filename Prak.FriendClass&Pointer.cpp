58c9365e5bea3e1cacd8143b88f07b67b0b7fa4e
NekoKaicho <adecta7@gmail.com>
2026-04-28 23:22:47 +0700

    Tambah deklarasi class BelahKetupat beserta membernya

--- Prak.FriendClass&Pointer.cpp
+++ Prak.FriendClass&Pointer.cpp
@@ -1,2 +1,15 @@
 #include <iostream>
 using namespace std;
+
+class BelahKetupat
+{
+private:
+    float sisi, d1, d2;
+public:
+    void input();
+    void luas();
+    void keliling();
+    void output();
+};
+
+
